f6f49fbf158727ffd83ec0b262a6a122d989afda
Rashidov Ruslan <rashidov_r@list.ru>
2020-09-11 13:35:57 +0300

    step-3

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const mushroomsReducer = (acc, curr) => {
 };
 
 const sizeReducer = (acc, curr) => {
-  if (curr.diametr > 30) {
+  if (curr.diametr >= 30) {
     acc.push(curr);
   }
   return acc;
